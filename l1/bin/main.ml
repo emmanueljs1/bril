2c505a203f4dd70627bee07c5b48f879baeb1222
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2025-01-30 00:06:16 -0500

    Implement algorithms for forming basic blocks and building CFGs

--- l1/bin/main.ml
+++ l1/bin/main.ml
@@ -1,6 +1,7 @@
 open Yojson
 
 module StrMap = Map.Make(String)
+module IntMap = Map.Make(Int)
 
 type ty = Prim of string | Param of (string * ty)
 
@@ -43,6 +44,14 @@ let empty_ins : ins =
 
 let jmp (lbl: string) : instr =
   Instr { empty_ins with op = "\"jmp\""; labels = Some [lbl] }
+
+let is_jmp (i: ins) : bool = i.op = "\"jmp\""
+
+let is_ret (i: ins) : bool = i.op = "\"ret\""
+
+let is_br (i: ins) : bool = i.op = "\"br\""
+
+let is_terminator (i: ins) : bool = is_jmp i || is_ret i || is_br i
 
 let str_of_json : Raw.t -> string = function
   | `Stringlit str -> str
@@ -167,12 +176,93 @@ let json_of_func (f: func) : Raw.t =
 let json_of_prog (p: prog) : Raw.t =
   `Assoc [("functions", `List (List.map json_of_func p))]
 
-let rec add_jumps : instr list -> instr list = function
-  | [] -> []
-  | Label s :: instrs -> jmp s :: Label s :: add_jumps instrs
-  | instr :: instrs -> instr :: add_jumps instrs
+type block = instr list
+
+type blocks = block IntMap.t
+
+type cfg =
+  { entry: int
+  ; exit: int
+  ; successors: (int list) IntMap.t
+  ; blocks: blocks
+  }
+
+let mk_blocks : instr list -> blocks * int =
+  let rec loop (b: block) (k: int) (bs: blocks) : instr list -> blocks * int = function
+    | Label s :: instrs ->
+        begin match b with
+        | [] -> loop [Label s] k bs instrs
+        | _ -> loop [Label s] (k + 1) (IntMap.add k (List.rev b) bs) instrs
+        end
+    | Instr ins :: instrs when is_terminator ins ->
+        loop [] (k + 1) (IntMap.add k (List.rev (Instr ins :: b)) bs) instrs
+    | instr :: instrs -> loop (instr :: b) k bs instrs
+    | [] ->
+        begin match b with
+        | [] ->
+            if IntMap.is_empty bs then
+              (IntMap.singleton 0 [], 1)
+            else
+              (bs, k)
+        | _ -> (IntMap.add k (List.rev b) bs, k + 1)
+        end
+  in
+  loop [] 0 IntMap.empty
+
+let mk_successors (bs: blocks) (size: int) : (int list) IntMap.t =
+  let lbls =
+    IntMap.fold (fun k b acc ->
+      match b with
+      | Label s :: _ -> StrMap.add s k acc
+      | _ -> acc
+    ) bs StrMap.empty
+  in
+  IntMap.mapi (fun k b ->
+    match List.rev b with
+    | Instr ins :: _ when is_jmp ins ->
+        [ins.labels |> Option.get |> List.hd |> fun s -> StrMap.find s lbls]
+    | Instr ins :: _ when is_br ins ->
+        ins.labels |> Option.get |> List.map (fun s -> StrMap.find s lbls)
+    | Instr ins :: _ when is_ret ins -> []
+    | _ -> if k + 1 = size then [] else [k + 1]
+  ) bs
+
+let compute_cfg (instrs: instr list) : cfg =
+  let (bs, size) = mk_blocks instrs in
+  let successors = mk_successors bs size in
+
+  let exits =
+    IntMap.filter (fun k _ ->
+      IntMap.find k successors |> List.is_empty
+    ) bs |> IntMap.to_list
+  in
+
+  match exits with
+  | [(k, _)] -> { entry = 0; exit = k; successors = successors; blocks = bs }
+  | _ ->
+      { entry = 0
+      ; exit = size
+      ; successors = IntMap.map (function | [] -> [size] | l -> l) successors
+      ; blocks = IntMap.add size [] bs
+      }
 
 let () =
   let p = Raw.from_file Sys.argv.(1) |> prog_of_json in
-  let p' = List.map (fun f -> { f with instrs = add_jumps f.instrs }) p in
-  json_of_prog p' |> Raw.to_string |> print_endline
+  let cfgs = List.map (fun f -> compute_cfg f.instrs) p in
+  List.iter (fun cfg ->
+    Printf.sprintf "entry: %d" cfg.entry |> print_endline;
+    Printf.sprintf "exit: %d" cfg.exit |> print_endline;
+    print_endline "blocks:";
+    IntMap.iter (fun k b ->
+      Printf.sprintf "block: %d" k |> print_endline;
+      List.iter (fun i ->
+        json_of_instr i |> Raw.to_string |> print_endline
+      ) b
+    ) cfg.blocks;
+    print_endline "edges:";
+    IntMap.iter (fun k l ->
+      List.iter (fun k' ->
+        Printf.sprintf "%d -> %d" k k' |> print_endline
+      ) l
+    ) cfg.successors
+  ) cfgs
